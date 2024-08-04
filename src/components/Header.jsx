74774cef6372b1243882cefee39bc2ae346f1f0c
Nicolas Rueda <164514047+nicktechz@users.noreply.github.com>
2024-08-04 13:02:25 -0500

    Added the file structure for header

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+  return (
+    <nav>
+      <section>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <ul>
+            <li>
+              <a href="">Creator</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </nav>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./styles/reset.css";
 import "./styles/fonts.css";
 import "./styles/index.css";
+import Header from "./components/Header";
 
 ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
+    <Header />
     <h1>Hola mundo</h1>
   </React.StrictMode>
 );
