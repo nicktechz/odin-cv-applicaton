6fd1979e53037f2262f7ba4bcf44431efa86a897
Nicolas Rueda <164514047+nicktechz@users.noreply.github.com>
2024-08-04 16:05:18 -0500

    Added some other structure for the configuration process

--- src/components/Configurator.jsx
+++ src/components/Configurator.jsx
@@ -1,7 +1,44 @@
+import "../styles/components/Configurator.css";
 export default function Configurator() {
   return (
-    <section style={{ width: "40%" }}>
-      <h2>Configuration:</h2>
+    <section className="configurator">
+      <h2 className="configuratorTitle">Configuration:</h2>
+      <aside className="card">
+        <div className="cardHeader">
+          <h3>Personal information</h3>
+          <img
+            src="src/assets/icons/chevron-up.svg"
+            alt=""
+            className="collapseIcon"
+          />
+        </div>
+        <div className="inputBox inputGroup">
+          <div>
+            <label htmlFor="">
+              First name: <span className="requiredIcon">*</span>
+            </label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">
+              Last Name: <span className="requiredIcon">*</span>
+            </label>
+            <input type="text" />
+          </div>
+        </div>
+        <div className="inputBox">
+          <label htmlFor="">
+            Email: <span className="requiredIcon">*</span>
+          </label>
+          <input type="email" />
+        </div>
+        <div className="inputBox">
+          <label htmlFor="">
+            Phone number: <span className="requiredIcon">*</span>
+          </label>
+          <input type="tel" />
+        </div>
+      </aside>
     </section>
   );
 }
